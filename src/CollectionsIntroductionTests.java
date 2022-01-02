620cdf6cf178fafd199f70182b89ee7a6d2638cb
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-02 21:13:35 +0200

    cw #26 with TODO's for hw #26

---
+++ src/CollectionsIntroductionTests.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+class CollectionsIntroductionTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void ImmutableCollection() {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4);
+		boolean flException = false;
+		Collection<Integer> list1 = new ArrayList<>(list);
+		list1 = Collections.unmodifiableCollection(list1);
+		try {
+			list1.add(10);
+		} catch (UnsupportedOperationException e) {
+			flException = true;
+		}
+		assertTrue(flException);
+	}
+	@Test
+	void sublistTest() {
+		List<Integer> list = new LinkedList<>(Arrays.asList(10,20,30,40,50));
+		List<Integer> sublist = list.subList(1, 3);
+		assertEquals(2, sublist.size());
+		assertArrayEquals(new Integer[] {20,30}, sublist.toArray(new Integer[2]));
+		sublist.add(0, -20);
+		sublist = list.subList(1, 3);
+		assertArrayEquals(new Integer[] {-20,20}, sublist.toArray(new Integer[2]));
+		sublist.add(-30);
+		sublist = list.subList(2, 4);
+		assertArrayEquals(new Integer[] {20,-30}, sublist.toArray(new Integer[2]));
+		
+	}
+
+}
---
+++ src/telran/util/MyStack.java
@@ -0,0 +1,40 @@
+package telran.util;
+
+/**
+ * 
+ * all specified below methods should be written with complexity O[1] 
+ *
+ */
+public class MyStack {
+//TODO fields
+	/*
+	 * removes the last element and returns it
+	 * if the stack is empty an exception "NoSuchElementException" should be thrown
+	 */
+	Integer pop () {
+		//TODO
+		return null;
+	}
+	/**
+	 * 
+	 * @return true it the stack is empty
+	 */
+	boolean isEmpty() {
+		//TODO
+		return false;
+	}
+	/**
+	 * adds the given element at the end of the stack
+	 * @param element
+	 */
+	void push(Integer element) {
+		//TODO
+	}
+	/**
+	 * 
+	 * @return maximal element existing in the stack
+	 */
+	Integer max() {
+		return null;
+	}
+}
